efe4b72a8ef5c3095d57b1259a51b63e3dc95d7e
Junya Ueda <uejun@users.noreply.github.com>
2024-11-18 11:58:59 +0900

    Update for windows

--- make_pdf.py
+++ make_pdf.py
@@ -1,12 +1,19 @@
+import os
+import platform
 import click
-
-from controller import CaptureController
 
 
 @click.command()
 @click.option('--name', '-n', type=str, default='Kindle', help="application name")
 @click.option('--direction', '-d', type=str, default='right', help="paging direction")
 def main(name, direction):
+    # Mac Winで切り替え
+    if platform.system() == 'Darwin':
+        from controller_mac import CaptureController
+    elif platform.system() == 'Windows':
+        from controller_win import CaptureController
+    else:
+        raise ValueError("Unsupported OS")
     controller = CaptureController(name, direction)
     controller.start()
 
